80ad9a85d94c4dbb7e864e18c1ecd75a629a82a2
Liyou Zhou <liyou.zhou@outlook.com>
2024-01-07 15:46:53 +0000

    Add pothole map to the report

--- assessment-1/src/pothole_inspection/src/report_generator_node.py
+++ assessment-1/src/pothole_inspection/src/report_generator_node.py
@@ -7,11 +7,17 @@ from datetime import datetime
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import ReentrantCallbackGroup
 from threading import Event
+from ament_index_python.packages import get_package_share_directory
 
 from pothole_inspection.srv import ReportAggregatedDetections, GenerateReport
 from cv_bridge import CvBridge
 import cv2
+import yaml
+from matplotlib import pyplot as plt
+from matplotlib.patches import Circle
+import numpy as np
 
+plt.switch_backend('agg')
 
 class ReportGeneratorNode(Node):
     def __init__(self):
@@ -40,15 +46,6 @@ class ReportGeneratorNode(Node):
 
         self.get_logger().info("requesting aggregated detections...")
         aggregated_detections = self.cli.call(req)
-        # future.add_done_callback(done_callback)
-
-        # Wait for future to be done
-        # self.get_logger().info("waiting for aggregated detections...")
-        # event.wait()
-
-        # at this point result is available
-        # self.get_logger().info("Retriving aggregated detections...")
-        # detections = future.result()
 
         report_path = request.path
         os.makedirs(report_path, exist_ok=True)
@@ -56,9 +53,27 @@ class ReportGeneratorNode(Node):
         now = datetime.now()  # current date and time
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-        report = f"# Pothole Inspection Report\n\n{date_time}\n\n## Potholes\n\n"
-        report += f"| ID | x | y | z | radius | image |\n"
-        report += f"| --- | --- | --- | --- | --- | --- |\n"
+        fig = plt.figure()
+        maps_folder = os.path.join(
+            get_package_share_directory("pothole_inspection"),
+            "maps",
+        )
+        map_name = "potholes_20mm"
+        play_area = plt.imread(f"{maps_folder}/{map_name}.pgm")
+        play_area_yaml = None
+        with open(f"{maps_folder}/{map_name}.yaml", "r") as fd:
+            play_area_yaml = yaml.safe_load(fd)
+        resolution = play_area_yaml["resolution"]  # m/pixel
+        origin = np.array(play_area_yaml["origin"])
+        plt.imshow(play_area, cmap="gray")
+        pothole_map_path = f"{report_path}/pothole_map.png"
+
+        report = f"# Pothole Inspection Report\n\n{date_time}\n\n"
+        report += "## Pothole Map\n\n"
+        report += f"![]({pothole_map_path})\n\n"
+        report += "## Potholes\n\n"
+        report += "| ID | x | y | z | radius | image |\n"
+        report += "| --- | --- | --- | --- | --- | --- |\n"
         for idx, pth in enumerate(aggregated_detections.potholes):
             self.get_logger().info(f"Pothole at {pth.x}, {pth.y}, {pth.z}")
 
@@ -68,6 +83,17 @@ class ReportGeneratorNode(Node):
 
             report += f"| {idx} | {pth.x:.04f} | {pth.y:.04f} | {pth.z:.04f} | {pth.radius:.04f} | ![]({idx}.png) |\n"
 
+            x = (pth.x - origin[0]) / resolution
+            y = (pth.y - origin[1]) / resolution
+            radius = pth.radius / resolution
+            c = Circle((x, y), radius, color="g", fill=False)
+            plt.gca().add_patch(c)
+
+            plt.text(x+1, y+0.5, f"{idx}", fontsize = 6, color="gray")
+
+        plt.gca().invert_xaxis()
+        plt.tight_layout()
+        plt.savefig(pothole_map_path)
         report_path = os.path.abspath(f"{report_path}/report.md")
         self.get_logger().info(f"Writing report to {report_path}")
         with open(report_path, "w") as f:
